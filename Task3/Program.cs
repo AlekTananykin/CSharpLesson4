1cee962ab5d972305f8f47c02e469cd317a050a8
AlekTananykin <tananykin@mail.ru>
2020-11-05 23:00:47 +0300

    Task 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -3,13 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task3Library;
 
+/*Тананыкин 
+ * а) Дописать класс для работы с одномерным массивом. Реализовать конструктор, 
+ * создающий массив определенного размера и заполняющий массив числами от 
+ * начального значения с заданным шагом. Создать свойство Sum, которое возвращает 
+ * сумму элементов массива, метод Inverse, возвращающий новый массив с 
+ * измененными знаками у всех элементов массива (старый массив, остается 
+ * без изменений),  метод Multi, умножающий каждый элемент массива на определённое
+ * число, свойство MaxCount, возвращающее количество максимальных элементов. 
+ * б)** Создать библиотеку содержащую класс для работы с массивом. Продемонстрировать работу библиотеки
+ * е) *** Подсчитать частоту вхождения каждого элемента в массив (коллекция Dictionary<int,int>)
+ */
 namespace Task3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Программа, демонстрирующая работу с классом " +
+                "массива MyArray, который объявлен в библиотеке классов " +
+                "Task3Library. ");
+            Console.WriteLine();
+
+            const int arraySize = 23;
+            const int firstElement = 2;
+            const int step = 3;
+            MyArray array = new MyArray(arraySize, firstElement, step);
+
+            Console.WriteLine("\nМассив чисел");
+            PrintMyArray(array);
+
+            Console.WriteLine("\nРазмер массива: {0}", array.Length);
+            Console.WriteLine("Максимальный элемент массива: {0}", array.Max);
+            Console.WriteLine("Минимальный элемент массива: {0}", array.Min);
+            Console.WriteLine("Количество максимальных элеметов: {0}", array.MaxCount);
+            Console.WriteLine("Сумма элементов массива: {0}", array.Sum);
+
+            Console.WriteLine("\nСоздан инвертированный массив:");
+            MyArray inverseArray = array.Inverse();
+            PrintMyArray(inverseArray);
+
+            const int factor = 3;
+            Console.WriteLine(
+                "\nУмножение исходного массива на число \"{0}\"", factor);
+
+            array.Multi(factor);
+            PrintMyArray(array);
+
+            Console.ReadKey();
+        }
+
+        private static void PrintMyArray(MyArray array)
+        {
+            for (int i = 0; i < array.Length; ++i)
+            {
+                Console.Write("{0}\t", array[i]);
+                if (0 == (i + 1) % 8)
+                    Console.WriteLine();
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ Task3Library/MyArray.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+
+namespace Task3Library
+{
+    public class MyArray
+    {
+        int[] _array;
+
+        public MyArray(int size)
+        {
+            _array = new int[size];
+        }
+
+        public MyArray(int size, int el)
+        {
+            _array = new int[size];
+            for (int i = 0; i < size; ++i)
+                _array[i] = el;
+        }
+
+        public MyArray(int size, int first, int step)
+        {
+            _array = new int[size];
+            for (int i = 0, val = first; i < size; ++i, val += step)
+                _array[i] = val;
+        }
+
+        public int Get(int pos)
+        {
+            return _array[pos];
+        }
+
+        public void Set(int pos, int value)
+        {
+            _array[pos] = value;
+        }
+
+        public int this[int pos]
+        {
+            get 
+            {
+                return _array[pos];
+            }
+            set 
+            {
+                _array[pos] = value;
+            }
+        }
+
+        public int Length
+        {
+            get
+            {
+                return _array.Length;
+            }
+        }
+
+        public int Max
+        {
+            get 
+            {
+                int max = _array[0];
+                for (int i = 1; i < _array.Length; ++i)
+                    if (_array[i] > max)
+                        max = _array[i];
+
+                return max;
+            }
+        }
+
+        public int Min
+        {
+            get {
+                int min = _array[0];
+                for (int i = 1; i < _array.Length; ++i)
+                    if (_array[i] < min)
+                        min = _array[i];
+
+                return min;
+            }
+        }
+
+        public int MaxCount
+        {
+            get
+            {
+                int max = this.Max;
+                int maxCount = 0;
+
+                foreach (int el in _array)
+                    if (max == el) ++ maxCount;
+
+                return maxCount;
+            }
+        }
+
+        public int Sum
+        {
+            get
+            {
+                int sum = 0;
+                for (int i = 0; i < _array.Length; ++i)
+                    sum += _array[i];
+
+                return sum;
+            }
+        }
+
+        public MyArray Inverse()
+        {
+            MyArray inverseArray = new MyArray(_array.Length);
+            for (int i = 0; i < inverseArray.Length; ++i)
+                inverseArray[i] = -_array[i];
+
+            return inverseArray;
+        }
+
+        public void Multi(int factor)
+        {
+            for (int i = 0; i < _array.Length; ++i)
+                _array[i] *= factor;
+        }
+
+    }
+}
